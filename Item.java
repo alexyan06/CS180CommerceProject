75c82c1a54f49925f343a0a1e7cf72845a14a803
alexyan06 <yan517@purdue.edu>
2025-03-30 15:19:27 -0400

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,39 @@
+public class Item {
+    private String name;
+    private double cost;
+    private String seller;
+
+    public Item(String name, double cost, String seller) {
+        this.name = name;
+        this.cost = cost;
+        this.seller = seller;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
